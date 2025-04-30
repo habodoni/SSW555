ef70bfdfc10157ef7257492c389a087e345a03f8
habodoni <habodoni@stevens.edu>
2025-04-30 17:13:11 -0400

    Add: reusable instruction popup scene and logic

---
+++ InstructionPopup/instruction_popup.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var card_text = $Panel/Label
+@onready var next_button = $Panel/Button
+
+var cards: Array = []
+var current_index := 0
+var on_done_callback = null
+
+func _ready():
+	next_button.pressed.connect(_on_next_pressed)
+
+func show_cards(new_cards: Array, callback = null) -> void:
+	cards = new_cards
+	current_index = 0
+	on_done_callback = callback
+	card_text.text = cards[current_index]
+	show()
+
+func _on_next_pressed() -> void:
+	current_index += 1
+	if current_index < cards.size():
+		card_text.text = cards[current_index]
+	else:
+		hide()
+		if on_done_callback:
+			on_done_callback.call()
